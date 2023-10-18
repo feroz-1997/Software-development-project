26d3404c7c31e981547d0215d9e825c752408031
s <121642179+666xs@users.noreply.github.com>
2023-10-18 18:54:31 +0200

    app.py

--- app.py
+++ app.py
@@ -1,65 +1,62 @@
 from flask import Flask, render_template, request, jsonify
-import requests
+import asyncio
+import aiohttp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Replace with your Edamam API credentials
 APP_ID = '9a291c98'
 APP_KEY = 'a6d2008620692aa8a57bdd6594ef5b8b'
 
-def get_recipes(ingredients, num_recipes, meal_type):
+async def fetch_recipe(session, ingredient, num_recipes, meal_type):
     base_url = 'https://api.edamam.com/search'
-    
+
     # Construct the API request parameters
     params = {
-        'q': ','.join(ingredients),  # Comma-separated list of ingredients
+        'q': ingredient,  # Single ingredient
         'app_id': APP_ID,
         'app_key': APP_KEY,
         'from': 0,  # Start from the first result
         'to': num_recipes,  # Get the specified number of recipes
     }
 
-    # Add meal_type as a filter if specified
     if meal_type:
-        params['mealType'] = meal_type
+        params['mealType'] = meal_type  # Add meal_type as a filter
 
-    try:
-        response = requests.get(base_url, params=params)
+    async with session.get(base_url, params=params) as response:
+        if response.status == 200:
+            data = await response.json()
+            return data
+        return None
 
-        if response.status_code == 200:
-            data = response.json()
+async def fetch_recipes(ingredients, num_recipes, meal_type):
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        tasks = [fetch_recipe(session, ingredient, num_recipes, meal_type) for ingredient in ingredients]
+        results = await asyncio.gather(*tasks)
+        return results
 
-            if 'hits' in data:
-                recipes = data['hits']
-                recipe_info = ""
-                for i, recipe in enumerate(recipes):
-                    recipe_data = recipe['recipe']
-                    recipe_info += f"Recipe {i + 1}: {recipe_data['label']}\n"
-                    recipe_info += f"Ingredients: {', '.join(recipe_data['ingredientLines'])}\n"
-                    recipe_info += f"Link: {recipe_data['url']}\n\n"
+def process_recipes(recipes):
+    processed_recipes = []
 
-                return recipe_info
-            else:
-                return "No recipes found."
+    for recipe_data in recipes:
+        if recipe_data and 'hits' in recipe_data:
+            hits = recipe_data['hits']
+            for hit in hits:
+                recipe = hit['recipe']
+                label = recipe['label']
+                ingredients = recipe.get('ingredientLines', [])  # Handle missing 'ingredientLines' gracefully
+                url = recipe.get('url', '')  # Handle missing 'url' gracefully
+                processed_recipes.append({
+                    'label': label,
+                    'ingredients': ingredients,
+                    'url': url
+                })
 
-        else:
-            return f"Failed to retrieve recipes. Status code: {response.status_code}"
-
-    except requests.exceptions.RequestException as e:
-        return f"An error occurred: {e}"
-
-def home():
-    # You can pass data to the template, e.g., recipes
-    recipes = [{'label': 'Recipe 1', 'ingredients': 'Ingredient 1, Ingredient 2', 'url': 'https://example.com/recipe1'},
-               {'label': 'Recipe 2', 'ingredients': 'Ingredient A, Ingredient B', 'url': 'https://example.com/recipe2'}]
-
-    return render_template('index.html', recipes=recipes)
-
+    return processed_recipes
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/search-recipes', methods=['POST'])
 def search_recipes():
@@ -68,8 +65,13 @@ def search_recipes():
     num_recipes = data.get('num_recipes', 10)
     meal_type = data.get('meal_type', None)  # Optional meal type
 
-    result = get_recipes(ingredients, num_recipes, meal_type)
-    return jsonify({'recipes': result})
+    # Fetch recipes asynchronously
+    recipes = asyncio.run(fetch_recipes(ingredients, num_recipes, meal_type))
+
+    # Process the recipes as needed
+    processed_recipes = process_recipes(recipes)
+
+    return jsonify({'recipes': processed_recipes})
 
 if __name__ == '__main__':
     app.run(debug=True)
